80c8c8facca3d8ff4a56c47a8b96b70d10edc206
huangzhibo <huangzhibo@hfjy.com>
2018-11-28 16:10:42 +0800

    register

--- 05/express_generator_demo/listing6_6/app.js
+++ 05/express_generator_demo/listing6_6/app.js
@@ -3,11 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const entries = require('./routes/entries');
 const validate = require('./middleware/validate');
+const register = require('./routes/register');
+const messages = require('./middleware/messages');
 
 var app = express();
 
@@ -19,6 +22,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(messages);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', indexRouter);
@@ -30,6 +39,8 @@ app.post('/post',
   validate.required('entry[title]'),
   validate.lengthAbove('entry[title]', 4),
   entries.submit);
+app.get('/register', register.form);
+app.post('/register', register.submit);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
